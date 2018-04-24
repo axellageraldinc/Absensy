8c696c79a15201344f3de4f928db208bd26a7013
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2018-04-24 19:50:37 +0700

    Added splash screen

---
+++ app/src/main/java/ppl/com/absensy/activity/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package ppl.com.absensy.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ppl.com.absensy.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
